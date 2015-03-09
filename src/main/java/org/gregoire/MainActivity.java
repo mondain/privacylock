c5778b2dc1c64ea89d1fef397dd2394286bf0571
Paul Gregoire <mondain@gmail.com>
2015-03-09 00:19:43 -0400

    Best working build yet

--- src/main/java/org/gregoire/MainActivity.java
+++ src/main/java/org/gregoire/MainActivity.java
@@ -1,18 +1,22 @@
 package org.gregoire;
 
+import java.util.List;
+
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.app.admin.DevicePolicyManager;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.PixelFormat;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
@@ -35,10 +39,12 @@ public class MainActivity extends Activity {
 		// start our service
 		startService(new Intent(this, LockScreenService.class));
 		// set up our lockscreen - A simple method that sets the screen to fullscreen. It removes the Notifications bar, the Actionbar and the virtual keys (if they are on the phone)
-		getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT | WindowManager.LayoutParams.TYPE_KEYGUARD);
+		//getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT | WindowManager.LayoutParams.TYPE_KEYGUARD);
+		getWindow().setType(WindowManager.LayoutParams.TYPE_SYSTEM_ALERT);
 		//getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		final int flags = WindowManager.LayoutParams.FLAG_FULLSCREEN | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
-		final int type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT | WindowManager.LayoutParams.TYPE_KEYGUARD;
+		//final int type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT | WindowManager.LayoutParams.TYPE_KEYGUARD;
+		final int type = WindowManager.LayoutParams.TYPE_SYSTEM_ALERT;
 		////Log.v(TAG, "Flags: " + flags);
 		DisplayMetrics metrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay().getMetrics(metrics);
@@ -50,20 +56,39 @@ public class MainActivity extends Activity {
 		WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
 		ViewGroup topView = (ViewGroup) getLayoutInflater().inflate(R.layout.activity_main, null);
 		// set visibility mod
-		if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.ICE_CREAM_SANDWICH) { // < 14
+		if (android.os.Build.VERSION.SDK_INT < 14) { //Build.VERSION_CODES.ICE_CREAM_SANDWICH) { // < 14
 			setUiVisibilityICS(topView);
-		} else if (android.os.Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) { // < 19
+		} else if (android.os.Build.VERSION.SDK_INT < 19) { //Build.VERSION_CODES.KITKAT) { // < 19
 			setUiVisibilityJB(topView);
 		} else {
 			setUiVisibility(topView);
 		}
 		// get the button
 		Button unlock = (Button) topView.findViewById(R.id.unlockBtn);
+		unlock.setOnClickListener(new OnClickListener() {
+
+			@Override
+            public void onClick(View view) {
+				Log.v(TAG, "onClick");
+				unlockScreen(view);
+            }
+
+		});
 		unlock.setOnTouchListener(new OnTouchListener() {
 
 			@Override
 			public boolean onTouch(View view, MotionEvent event) {
-				unlockScreen(view);
+				Log.v(TAG, "onTouch");    
+				switch (event.getAction()) {
+				    case MotionEvent.ACTION_DOWN:
+				        //some code....
+				        break;
+				    case MotionEvent.ACTION_UP:
+				    	view.performClick();
+				        break;
+				    default:
+				        break;
+				    }
 				return true;
 			}
 
@@ -77,19 +102,19 @@ public class MainActivity extends Activity {
 		adminReceiverName = new ComponentName(this, AdminReceiver.class);
 	}
 
-	@TargetApi(Build.VERSION_CODES.KITKAT)
+	@TargetApi(19) // Build.VERSION_CODES.KITKAT
 	private void setUiVisibility(View topView) {
 		Log.v(TAG, "Default >= KitKat");
 		topView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE);
 	}
 
-	@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
-	private void setUiVisibilityJB(View topView) {
+	@TargetApi(18) // Build.VERSION_CODES.JELLY_BEAN_MR2
+ 	private void setUiVisibilityJB(View topView) {
 		Log.v(TAG, "JellyBean");
 		topView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LOW_PROFILE);
 	}
 
-	@TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+	@TargetApi(14) // Build.VERSION_CODES.ICE_CREAM_SANDWICH
 	private void setUiVisibilityICS(View topView) {
 		Log.v(TAG, "IceCreamSandwich");
 
@@ -155,8 +180,30 @@ public class MainActivity extends Activity {
 		return; //Do nothing!
 	}
 
+	private void startAndroidLauncher() {
+		PackageManager pm = getPackageManager();
+		Intent i = new Intent("android.intent.action.MAIN");
+		i.addCategory("android.intent.category.HOME");
+		List<ResolveInfo> lst = pm.queryIntentActivities(i, 0);
+		if (lst != null) {
+			for (ResolveInfo resolveInfo : lst) {
+				try {
+					Log.v(TAG, "Package: " + resolveInfo.activityInfo.packageName + " name: " + resolveInfo.activityInfo.name);
+					Intent home = new Intent("android.intent.action.MAIN");
+					home.addCategory("android.intent.category.HOME");
+					home.setClassName(resolveInfo.activityInfo.packageName, resolveInfo.activityInfo.name);
+					startActivity(home);
+					break;
+				} catch (Throwable t) {
+					t.printStackTrace();
+				}
+			}
+		}
+	}
+
 	public void unlockScreen(View view) {
 		Log.v(TAG, "unlockScreen ");
+		startAndroidLauncher();
 		//Instead of using finish(), this totally destroys the process
 		android.os.Process.killProcess(android.os.Process.myPid());
 		this.finish();
